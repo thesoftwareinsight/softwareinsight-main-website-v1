2b945114009167b772fdca3e02edda34c4a2d561
Muzammil Jumshaid <thisismjey@gmail.com>
2022-09-20 23:03:25 +0500

    updated the meta tags

--- index.php
+++ index.php
@@ -1,25 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="UTF-8">
-		<meta name="keywords" content="Digital marketing agency, Digital marketing company, Digital marketing services, sass, software company">
-		<meta name="description" content="vCamp Creative Personal Service HTML Template designed for personal service.">
-      	<meta property="og:site_name" content="vCamp">
-      	<meta property="og:url" content="https://heloshape.com/">
-      	<meta property="og:type" content="website">
-      	<meta property="og:title" content="The Software Insight - Full services software development company">
-		<meta name='og:image' content='images/assets/ogg.png'>
-		<!-- For IE -->
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<!-- For Resposive Device -->
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<!-- For Window Tab Color -->
-		<!-- Chrome, Firefox OS and Opera -->
-		<meta name="theme-color" content="#202020">
-		<!-- Windows Phone -->
-		<meta name="msapplication-navbutton-color" content="#202020">
-		<!-- iOS Safari -->
-		<meta name="apple-mobile-web-app-status-bar-style" content="#202020">
+		<!-- Primary Meta Tags -->
+        <title>The Software Insight - Full-services software development company</title>
+        <meta name="title" content="The Software Insight - Full-services software development company">
+        <meta name="description" content="We are full-service software development company that specializes in creating scalable and responsive solutions that are tailored to the client’s specific requirements. We specialize in creating innovative web and mobile applications that are fast, secure, scalable, and simple to operate for companies of all sizes, from start-ups to well-established corporations.
+        ">
+
+        <!-- Open Graph / Facebook -->
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="https://thesoftwareinsight.com/">
+        <meta property="og:title" content="The Software Insight - Full-services software development company">
+        <meta property="og:description" content="We are full-service software development company that specializes in creating scalable and responsive solutions that are tailored to the client’s specific requirements. We specialize in creating innovative web and mobile applications that are fast, secure, scalable, and simple to operate for companies of all sizes, from start-ups to well-established corporations.
+        ">
+        <meta property="og:image" content="">
+
+        <!-- Twitter -->
+        <meta property="twitter:card" content="summary_large_image">
+        <meta property="twitter:url" content="https://thesoftwareinsight.com/">
+        <meta property="twitter:title" content="The Software Insight - Full-services software development company">
+        <meta property="twitter:description" content="We are full-service software development company that specializes in creating scalable and responsive solutions that are tailored to the client’s specific requirements. We specialize in creating innovative web and mobile applications that are fast, secure, scalable, and simple to operate for companies of all sizes, from start-ups to well-established corporations.
+        ">
+        <meta property="twitter:image" content="">
+        
 		<title>The Software Insight - Full services software development company</title>
 		<!-- Favicon -->
 		<link rel="icon" type="image/png" sizes="56x56" href="images/fav-icon/icon.png">
@@ -27,13 +30,6 @@
 		<link rel="stylesheet" type="text/css" href="css/style.css" media="all">
 		<!-- responsive style sheet -->
 		<link rel="stylesheet" type="text/css" href="css/responsive.css" media="all">
-
-		<!-- Fix Internet Explorer ______________________________________-->
-		<!--[if lt IE 9]>
-			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-			<script src="vendor/html5shiv.js"></script>
-			<script src="vendor/respond.js"></script>
-		<![endif]-->	
 	</head>
 
 	<body>
